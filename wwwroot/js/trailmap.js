ae91d9fe8910a8d552fc9d5631054e950367215c
Andrew Carver <Andrew.Carver.dev@gmail.com>
2026-07-12 06:18:06 -0400

    cleanup

--- wwwroot/js/trailmap.js
+++ wwwroot/js/trailmap.js
@@ -23,97 +23,6 @@ function drawPath(startPoint, endPoint) {
     }).addTo(map);
 }
 
-// function getPointAlongMultiLine(multiLineGeoJSON, targetDistance, options = { units: 'miles' }) {
-//   const flattened = turf.flatten(multiLineGeoJSON);
-//   let rawSegments = [...flattened.features]; // Shallow copy to prevent mutating original data
-
-//   if (rawSegments.length === 0) return null;
-
-//   // 1. FIND THE TRUE STARTING POINT (Lowest Latitude / Southernmost Segment)
-//   let lowestLat = Infinity;
-//   let startIdx = 0;
-
-//   rawSegments.forEach((seg, idx) => {
-//     const coords = seg.geometry.coordinates;
-//     const startLat = coords[0][1];
-//     const endLat = coords[coords.length - 1][1];
-//     const minLat = Math.min(startLat, endLat);
-    
-//     if (minLat < lowestLat) {
-//       lowestLat = minLat;
-//       startIdx = idx;
-//     }
-//   });
-
-//   // Extract the true southern start segment (Fixing the splice array bug using [0])
-//   let currentSegment = rawSegments.splice(startIdx, 1)[0];
-  
-//   // Ensure the starting segment points Northbound
-//   if (currentSegment.geometry.coordinates[0][1] > currentSegment.geometry.coordinates[currentSegment.geometry.coordinates.length - 1][1]) {
-//     currentSegment.geometry.coordinates.reverse();
-//   }
-
-//   const sortedSegments = [currentSegment];
-
-//   // 2. CHAIN LINK GEOGRAPHIC SORTING
-//   while (rawSegments.length > 0) {
-//     const currentCoords = currentSegment.geometry.coordinates;
-//     const endPoint = turf.point(currentCoords[currentCoords.length - 1]);
-
-//     let closestIdx = 0;
-//     let minDistance = Infinity;
-//     let shouldFlip = false;
-
-//     for (let i = 0; i < rawSegments.length; i++) {
-//       const segCoords = rawSegments[i].geometry.coordinates;
-//       const startOfNext = turf.point(segCoords[0]);
-//       const endOfNext = turf.point(segCoords[segCoords.length - 1]);
-
-//       const distToStart = turf.distance(endPoint, startOfNext, options);
-//       const distToEnd = turf.distance(endPoint, endOfNext, options);
-
-//       if (distToStart < minDistance) {
-//         minDistance = distToStart;
-//         closestIdx = i;
-//         shouldFlip = false;
-//       }
-//       if (distToEnd < minDistance) {
-//         minDistance = distToEnd;
-//         closestIdx = i;
-//         shouldFlip = true;
-//       }
-//     }
-
-//     // CRITICAL FIX: Extract the actual object out of the spliced array
-//     currentSegment = rawSegments.splice(closestIdx, 1)[0];
-
-//     if (shouldFlip) {
-//       currentSegment.geometry.coordinates.reverse();
-//     }
-
-//     sortedSegments.push(currentSegment);
-//   }
-
-//   // 3. CHRONOLOGICAL MILEAGE TRACKING
-//   let accumulatedDistance = 0;
-
-//   for (const segment of sortedSegments) {
-//     const segmentLength = turf.length(segment, options);
-
-//     if (accumulatedDistance + segmentLength >= targetDistance) {
-//       const remainingDistanceNeeded = targetDistance - accumulatedDistance;
-//       return turf.along(segment, remainingDistanceNeeded, options);
-//     }
-
-//     accumulatedDistance += segmentLength;
-//   }
-
-//   // Fallback: Total trail distance check / Final endpoint
-//   console.log("Total Sorted Trail Length:", accumulatedDistance);
-//   const lastSegment = sortedSegments[sortedSegments.length - 1];
-//   const lastCoords = lastSegment.geometry.coordinates;
-//   return turf.point(lastCoords[lastCoords.length - 1]);
-// }
 
 function getPointAlongMultiLine(trailJson, targetDistance, options = { units: 'miles' }) {
   const flattened = turf.flatten(trailJson);
@@ -121,23 +30,18 @@ function getPointAlongMultiLine(trailJson, targetDistance, options = { units: 'm
 
   let accumulatedDistance = 0;
 
-  // 2. Loop sequentially through each line segment
   for (const segment of segments) {
     const segmentLength = turf.length(segment, options);
 
-    // 3. Check if our target distance lands inside this specific segment
     if (accumulatedDistance + segmentLength >= targetDistance) {
       const remainingDistanceNeeded = targetDistance - accumulatedDistance;
       
-      // Call standard turf.along safely on just this matching single segment
       return turf.along(segment, remainingDistanceNeeded, options);
     }
 
-    // Otherwise, add this segment's length and move to the next one
     accumulatedDistance += segmentLength;
   }
 
-  // Fallback: If target distance overshoots the total trail, return the final endpoint
   const lastSegment = segments[segments.length - 1];
   const lastCoords = lastSegment.geometry.coordinates;
   return turf.point(lastCoords[lastCoords.length - 1]);
